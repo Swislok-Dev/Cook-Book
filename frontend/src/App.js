c27d080362442300431264af0308f13cd42134f8
Swislok-Dev <cmarker4@gmail.com>
2022-05-25 08:53:00 -0500

    implement routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Header from './components/Header.jsx'
 
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
-  );
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
