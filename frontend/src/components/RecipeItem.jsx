897aeffec3ffd40e9ebe9f3c5e54ad3374d6dfa9
Swislok-Dev <cmarker4@gmail.com>
2022-06-03 12:14:17 -0500

    refactor recipe item delete button

--- frontend/src/components/RecipeItem.jsx
+++ frontend/src/components/RecipeItem.jsx
@@ -1,18 +1,29 @@
 import { useDispatch } from 'react-redux'
+import { useLocation } from 'react-router-dom'
 import { deleteRecipe } from '../features/recipes/recipeSlice.js'
 
 function RecipeItem({ recipe }) {
+  const location = useLocation()
+
   const dispatch = useDispatch()
+
+  const deleteButton = () => {
+    if (location.pathname === '/dashboard') {
+      return (
+        <button
+          onClick={() => dispatch(deleteRecipe(recipe._id))}
+          className='close'
+        >
+          X
+        </button>
+      )
+    }
+  }
   return (
     <div className='recipe'>
       <div>{new Date(recipe.createdAt).toLocaleString('en-US')}</div>
       <h2>{recipe.name}</h2>
-      <button
-        onClick={() => dispatch(deleteRecipe(recipe._id))}
-        className='close'
-      >
-        X
-      </button>
+      {deleteButton()}
     </div>
   )
 }
